2e68fa000c3dba49ddffbebdb34673d664af6af7
aravindan venkataraju <aravindan.venkataraju@gmail.com>
2022-07-17 05:16:43 +1000

    Initial commit

---
+++ test/cdk-rds-oracle-ts.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkRdsOracleTs from '../lib/cdk-rds-oracle-ts-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-rds-oracle-ts-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkRdsOracleTs.CdkRdsOracleTsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
